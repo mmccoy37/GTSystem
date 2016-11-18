21983c31d3b33b8e09b032d5d68af6cf354673bd
hdo33 <hdo33@gatech.edu>
2016-11-17 22:08:00 -0500

    add create user function

--- app/src/main/java/com/example/hungdo/team44phase3/Signup.java
+++ app/src/main/java/com/example/hungdo/team44phase3/Signup.java
@@ -1,15 +1,9 @@
 package com.example.hungdo.team44phase3;
 
-import android.database.SQLException;
-import android.database.sqlite.SQLiteConstraintException;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
