98c5d1c940a6f48f828d0f6ee65cc722c7af9ca7
qnguyen29 <qnnguyen29@gmail.com>
2016-11-27 05:33:02 -0500

    1

--- app/src/main/java/com/example/hungdo/team44phase3/MyApplicationScreen.java
+++ app/src/main/java/com/example/hungdo/team44phase3/MyApplicationScreen.java
@@ -23,7 +23,6 @@ public class MyApplicationScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myapp);
-
     }
 
 }
