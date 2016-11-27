1456352457ed454c38607e3b89ef0cd1012e2d8c
hdo33 <hdo33@gatech.edu>
2016-11-26 21:51:22 -0500

    add all student screen

---
+++ app/src/main/java/com/example/hungdo/team44phase3/CourseInfoScreen.java
@@ -0,0 +1,13 @@
+package com.example.hungdo.team44phase3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CourseInfoScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_info_screen);
+    }
+}
